e0ff6ce9a8e376d4100c597c3c993e3459de20db
sumant30 <sumant30@gmail.com>
2016-11-07 20:14:34 +0530

    Showing and hiding elements.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from 'angular2/core';
 import { CoursesComponent } from './courses.component';
 import { AuthorsComponent } from './authors.component';
+import { StarComponent } from './star.component';
 
 @Component({
     selector: 'my-app',
     template: `<h1>My First Angular 2 </h1>
                 <courses></courses>
                 <authors></authors>
+                <star></star>
                 `,
-    directives: [CoursesComponent,AuthorsComponent]
+    directives: [CoursesComponent,AuthorsComponent,StarComponent]
 })
 export class AppComponent { }
---
+++ app/star.component.ts
@@ -0,0 +1,19 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'star',
+    template:
+    `<div (click)='onClick($event)' [style.display]='isHidden ? "block" : "none"' class='glyphicon glyphicon-star-empty'></div>
+    <div (click)='onClick($event)'  [style.display]='!isHidden ? "block" : "none"' class='glyphicon glyphicon-star'></div>
+   `
+
+})
+
+export class StarComponent {
+
+    isHidden: boolean = true;
+
+    onClick($event) {
+        this.isHidden = !this.isHidden;
+    }
+}
